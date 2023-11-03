833ce418711b853f86581e5d6f886ea5ac16d871
Nishantjha786 <36167637+Nishantjha786@users.noreply.github.com>
2023-11-03 16:43:18 +0530

    reating signup screen

--- expense/app.js
+++ expense/app.js
@@ -1,89 +1,87 @@
 const path = require('path');
 var express = require('express');
 var app = express();
-//const express = require('express');
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
-//const app = express();
 const db  =  require('./connection');
 
-//const Sequelize = require('sequelize');
-//const sequelize = require('./connection');
+// const express  = require('express');
+// const path = require('path');
+// const fs = require('fs');
+// //const helmet = require('helmet');
+// //const morgan = require('morgan');
+// //const compression = require('compression');
+// const app = express();
+// const body_parser = require('body-parser');
+// //const dotenv = require('dotenv');
+// //dotenv.config();
 
+// const User = require('./model/user');
 
-
+const User = require('./model/user');
+const userController = require('./controllers/user');
 
 app.get('/',function(req,res){
     res.sendFile(__dirname+'/registration.html');
 });
-app.post('/',function(req,res){
-    // var date = req.body.start;
-    // var att = req.body.vehicle1;
-    // var phone = req.body.number;
-    var name = req.body.name;
-    // console.log(date);
-    // console.log(att);
-    console.log(name);
-
-});
-    // const Product = sequelize.define(columnname,{
-    //     id:{
-    //         type:Sequelize.INTEGER,
-    //         autoIncrement:true,
-    //         allowNull:false,
-    //         primaryKey:true
-    //     },
-    //     columnname:Sequelize.STRING
-    
-    // });
-    // sequelize.sync({
-    //     force: true
-    // })
-    // .then(result=>{
-    //     console.log(result);
-       
-    // })
-    // .catch(err=>{
-    //     console.log(err);
-    // });
-//     db.execute('insert into student (name,email,phone) values(?,?,?)',[name,email,phone])
-//     .then(res=>{
-// console.log(res);
-//     })
-//     .catch(err=>{
-// console.log(err);
+// app.post('/',function(req,res){
+//     var name = req.body.name;
+//     var email= req.body.email;
+//     var password = req.body.password;
+//  console.log("name  is "+name);
  
 
-    
-  //  module.exports=Product;
-  
-    
-    // con.connect(function(err){
-    //   if(err)throw err;
-    //   var sql= "insert into student values(name ,email,phone) values ?";
-    //   var values = [[name,email,phone]];
-    //   con.query(sql,values,function(error,result){
-    //     if(error)throw error;
-    //     res.send('student regitsration sucesful'+result.insertId);
-    //   });
-    // });
-
-
-// app.set('view engine', 'ejs');
-// app.set('views', 'views');
-
-// const adminData = require('./routes/admin');
-// const shopRoutes = require('./routes/shop');
-
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(express.static(path.join(__dirname, 'public')));
-
-// app.use('/admin', adminData.routes);
-// app.use(shopRoutes);
-
-// app.use((req, res, next) => {
-//   res.status(404).render('404', { pageTitle: 'Page Not Found' });
 // });
-app.listen(3000);
-//mapp.listen(3000);
+
+app.post('/', userController.addUser);
+  
+
+
+//const Expense = require('./models/expense');
+//const PremiumUser = require('./models/premium-user');
+//const Forgotpassword = require('./models/forgot-password');
+//onst Download = require('./models/download');
+
+// User.hasMany(Expense);
+// Expense.belongsTo(User);
+
+// User.hasOne(PremiumUser);
+// PremiumUser.belongsTo(User);
+
+// User.hasMany(Forgotpassword);
+// Forgotpassword.belongsTo(User);
+
+// User.hasMany(Download);
+// Download.belongsTo(User);
+
+// const userRoutes = require('./routes/user');
+// // const expenseRoutes = require('./routes/expense');
+// // const passwordRoutes = require('./routes/password');
+
+
+// const data_base = require('./util/database');
+
+// const cors = require('cors');
+
+// app.use(body_parser.json());
+// app.use(cors());
+
+// const accessLogStream = fs.createWriteStream(
+//     path.join(__dirname, 'access.log'),
+//     {flags: 'a'}
+// );
+
+// // app.use(helmet());
+// // app.use(compression());
+// // app.use(morgan('combined', {stream: accessLogStream}));
+
+// app.use('/user', userRoutes);
+// // app.use('/expense', expenseRoutes);
+// // app.use('/password', passwordRoutes);
+
+// // console.log(process.env.NODE_ENV);
+
+// // data_base.sync({force: true})
+
+app.listen(process.env.PORT || 4000);
--- expense/util/database.js
+++ expense/util/database.js
@@ -1,8 +1,20 @@
-const mysql = require("mysql");
-var con = mysql.createConnection({
-    host :"localhost",
-    user:"root",
-    password:"6424",
-    database:"shop"
-});
-module.exports= con;
+// const mysql = require("mysql");
+// var con = mysql.createConnection({
+//     host :"localhost",
+//     user:"root",
+//     password:"6424",
+//     database:"shop"
+// });
+// module.exports= con;
+// const Sequelize = require('sequelize');
+
+// const data_base = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+//     dialect: 'mysql',
+//     host: process.env.DB_HOST
+//   });
+  
+// module.exports = data_base;
+
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('shop','root','6424',{dialect:'mysql',host:'localhost'});
+module.exports = sequelize;
